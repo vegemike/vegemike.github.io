c4f1652d57757e45335259878fb21f7b751bc33b
Michael <mikeystradling@gmail.com>
2024-11-03 14:32:37 +0000

    update index.html and home.js

--- scripts/home.js
+++ scripts/home.js
@@ -5,3 +5,18 @@ document.querySelectorAll('.dir-btn').forEach(button => {
     });
   });
   
+function getModifiedDate(path){
+  fetch(`https://api.github.com/repos/vegemike/vegemike.github.io/commits?path=${path}&per_page=1`)
+  .then(response => response.json())
+  .then(data => {
+    if (data.length > 0) {
+      const lastModifiedDate = data[0].commit.committer.date;
+      console.log('Last Modified Date:', lastModifiedDate);
+    } else {
+      console.log('No commits found for this file.');
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+}
