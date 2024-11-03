1b058f25519761f14a1fa3e9de5fb13ba7a9c5b1
Michael <mikeystradling@gmail.com>
2024-11-03 20:39:41 +0000

    update music.html and terminal.css

--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,25 +1,2 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    const commandElement = document.querySelector('.command');
-    const cursorElement = document.createElement('span');
-    cursorElement.classList.add('cursor');
-    commandElement.appendChild(cursorElement);
-
-    // Example typing animation
-    const message = "> Disengage Lock";
-    let index = 0;
-    const typingSpeed = 100;
-
-    function typeText() {
-        if (index < message.length) {
-            commandElement.textContent += message.charAt(index);
-            index++;
-            setTimeout(typeText, typingSpeed);
-        } else {
-            commandElement.appendChild(cursorElement);
-        }
-    }
-
-    typeText();
-});
 
