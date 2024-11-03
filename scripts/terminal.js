2d87b6e7201cdbd874f070309de038af2a023d19
Michael <mikeystradling@gmail.com>
2024-11-03 20:44:01 +0000

    update music.html and terminal.js

--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,2 +1,13 @@
+const parentDiv = document.getElementById("commands")
+const commands = Array.from(parentDiv.children)
+highlightedCommand = 0
 
-
+function handleMouseOver(event) {
+    commands.forEach(child => {
+        child.classList.remove('command');
+    });
+    event.target.classList.add('command');
+}
+commands.forEach(child => {
+    child.addEventListener('mouseover', handleMouseOver);
+});
